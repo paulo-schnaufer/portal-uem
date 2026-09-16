0880a3dd8b58f0019d34357b6c85b6e18633d33b
Paulo Schnaufer <pauloschnaufer@gmail.com>
2026-09-16 12:22:44 -0300

    Adição de facilidades e oportunidades

--- src/data/facilidades.js
+++ src/data/facilidades.js
@@ -30,9 +30,63 @@ export const facilidadesData = [
     id: "instituto-de-linguas",
     titulo: "Instituto de Línguas (ILG)",
     categoria: "Idiomas",
-    descricao: "Cursos de línguas estrangeiras com preços acessíveis para a comunidade acadêmica.",
+    descricao: "Oferece cursos de idiomas, disponibilizando aulas de inglês, espanhol e francês, com foco na formação no idioma. Os cursos são oferecidos mediante pagamento de matrícula e mensalidades, que variam de R$105,70 a R$235,00.",
     link: "http://www.ilg.uem.br",
-    tags: ["inglês", "espanhol", "idiomas", "cursos"],
+    tags: ["inglês", "espanhol", "francês", "idiomas", "cursos"],
+    destaque: false
+  },
+  {
+    id: "propae",
+    titulo: "Programa de Apoio aos Estudantes com Necessidades Especiais (PROPAE)",
+    categoria: "Assistência Estudantil / Acessibilidade",
+    descricao: "Programa da UEM que oferece apoio a estudantes com deficiência e/ou necessidades educacionais especiais. Promove a inclusão, oferece materiais adaptados, acompanhamento e atua na redução de barreiras de acessibilidade.",
+    link: "http://www.propae.uem.br",
+    tags: ["acessibilidade", "inclusão", "deficiência", "apoio", "necessidades especiais"],
+    destaque: false
+  },
+  {
+    id: "instituto-de-estudos-japoneses",
+    titulo: "Instituto de Estudos Japoneses (IEJ)",
+    categoria: "Idiomas e Cultura",
+    descricao: "Promove a integração universidade-sociedade através de atividades relacionadas à Língua e Cultura Japonesa, trabalhando para difundir a cultura japonesa.",
+    link: "http://www.iej.uem.br",
+    tags: ["japonês", "cultura", "idiomas", "língua japonesa"],
+    destaque: false
+  },
+  {
+    id: "diretoria-assuntos-academicos",
+    titulo: "Diretoria de Assuntos Acadêmicos (DAA)",
+    categoria: "Assuntos Acadêmicos",
+    descricao: "Setor administrativo da vida do aluno na UEM. Na DAA, o estudante resolve questões de matrícula, solicita histórico escolar, emite certificados e acompanha toda a sua trajetória documental.",
+    link: "http://www.daa.uem.br",
+    tags: ["matrícula", "histórico escolar", "certificados", "documentos", "graduação"],
+    destaque: false
+  },
+  {
+    id: "auxilio-alimentacao",
+    titulo: "Auxílio Alimentação",
+    categoria: "Assistência Estudantil",
+    descricao: "Concedido aos alunos em situação de vulnerabilidade socioeconômica, com a finalidade de oferecer condições para o atendimento de suas necessidades de alimentação básica e contribuir para a sua permanência na instituição.",
+    link: "http://www.dca.uem.br",
+    tags: ["alimentação", "auxílio", "vulnerabilidade", "permanência", "assistência"],
+    destaque: false
+  },
+  {
+    id: "central-de-estagio",
+    titulo: "Central de Estágio",
+    categoria: "Oportunidades / Estágios",
+    descricao: "Através da Central de Estágio do governo do Paraná, oferece estágio remunerado não obrigatório no campus da UEM. As vagas são abertas para alunos de graduação de todas as instituições de ensino.",
+    link: "http://www.estagio.uem.br",
+    tags: ["estágio", "remunerado", "vagas", "governo pr", "oportunidade"],
+    destaque: false
+  },
+  {
+    id: "servico-social-e-psicologico",
+    titulo: "Serviço Social e Psicológico (ACOLHE UEM)",
+    categoria: "Assistência Estudantil",
+    descricao: "Desenvolve ações para atender demandas psicossociais e socioeconômicas, contribuindo para a permanência do aluno. Inclui o projeto ACOLHE UEM, que orienta estudantes sobre serviços e direitos por meio de rodas de conversa.",
+    link: "http://www.dca.uem.br",
+    tags: ["psicologia", "serviço social", "acolhe uem", "saúde mental", "apoio"],
     destaque: false
   }
 ];
--- src/data/oportunidades.js
+++ src/data/oportunidades.js
@@ -3,10 +3,37 @@ export const oportunidadesData = [
     id: "iniciacao-cientifica",
     titulo: "Iniciação Científica (PPG)",
     categoria: "Pesquisa",
-    descricao: "Editais e orientações para bolsas de pesquisa PIBIC, PIC e PIBITI na universidade.",
+    descricao: "Editais e orientações gerais para bolsas de pesquisa, inovação e projetos científicos na universidade.",
     link: "http://www.ppg.uem.br",
     tags: ["pesquisa", "pibic", "bolsas", "ciência"],
     destaque: true
+  },
+  {
+    id: "pic-iniciacao-cientifica",
+    titulo: "Programa de Iniciação Científica (PIC)",
+    categoria: "Pesquisa",
+    descricao: "Estimula a pesquisa voluntária na graduação por 12 meses (12h/semana) sob orientação docente, culminando em apresentação de resultados científicos.",
+    link: "http://www.ppg.uem.br",
+    tags: ["pesquisa", "pic", "voluntário", "ciência"],
+    destaque: false
+  },
+  {
+    id: "pibic-bolsas-pesquisa",
+    titulo: "Programa Institucional de Bolsas de Iniciação Científica (PIBIC)",
+    categoria: "Pesquisa",
+    descricao: "Programa remunerado (20h/semana por 12 meses) para desenvolvimento de pesquisas com coleta, análise de dados e apresentação dos resultados no EAIC.",
+    link: "http://www.ppg.uem.br",
+    tags: ["pesquisa", "pibic", "bolsas", "eaic", "ciência"],
+    destaque: true
+  },
+  {
+    id: "pibiti-tecnologia-inovacao",
+    titulo: "Programa de Iniciação em Desenvolvimento Tecnológico e Inovação (PIBITI)",
+    categoria: "Pesquisa e Inovação",
+    descricao: "Bolsas focadas no desenvolvimento de novas tecnologias, inovação, propriedade intelectual e empreendedorismo (20h/semana por 12 meses) com apresentação no EAIC.",
+    link: "http://www.ppg.uem.br",
+    tags: ["tecnologia", "inovação", "pibiti", "bolsas", "empreendedorismo"],
+    destaque: false
   },
   {
     id: "projetos-extensao",
@@ -18,13 +45,22 @@ export const oportunidadesData = [
     destaque: false
   },
   {
-    id: "escritorio-cooperacao-internacional",
-    titulo: "Escritório de Cooperação Internacional (ECI)",
+    id: "mobilidade-academica-internacional",
+    titulo: "Mobilidade Acadêmica Internacional (ECI)",
     categoria: "Intercâmbio",
-    descricao: "Oportunidades de mobilidade acadêmica, bolsas no exterior e parcerias internacionais.",
+    descricao: "Coordenada pelo ECI, promove acordos com instituições estrangeiras, bolsas no exterior, recepção de alunos internacionais e eventos como o Mobility Day.",
     link: "http://www.eci.uem.br",
-    tags: ["intercâmbio", "viagem", "exterior", "mobilidade"],
+    tags: ["intercâmbio", "mobilidade", "exterior", "internacional", "mobility day"],
     destaque: true
+  },
+  {
+    id: "internacionalizacao-em-casa",
+    titulo: "Internacionalização em Casa (IeC)",
+    categoria: "Intercâmbio e Idiomas",
+    descricao: "Promove experiências internacionais no campus por meio de políticas linguísticas, Português para Estrangeiros (PLA), Celpe-Bras e suporte a intercâmbios virtuais (COIL).",
+    link: "http://www.eci.uem.br",
+    tags: ["iec", "português", "celpe-bras", "coil", "internacional"],
+    destaque: false
   },
   {
     id: "secao-estagios",
@@ -35,4 +71,49 @@ export const oportunidadesData = [
     tags: ["estágio", "emprego", "carreira", "prática"],
     destaque: false
   },
+  {
+    id: "diretoria-assuntos-academicos",
+    titulo: "Diretoria de Assuntos Acadêmicos (DAA)",
+    categoria: "Assuntos Acadêmicos",
+    descricao: "Setor administrativo do aluno na UEM. Gestão de matrícula, solicitação de histórico escolar, emissão de certificados e trajetória documental até a graduação.",
+    link: "http://www.daa.uem.br",
+    tags: ["matrícula", "histórico escolar", "certificados", "documentos"],
+    destaque: false
+  },
+  {
+    id: "empresas-juniores",
+    titulo: "Empresas Juniores (EJ)",
+    categoria: "Empreendedorismo / Prática Profissional",
+    descricao: "Associações geridas por estudantes que prestam consultoria real com orientação docente, proporcionando vivência empresarial prática na graduação.",
+    link: "http://www.uem.br",
+    tags: ["empresa júnior", "consultoria", "empreendedorismo", "prática profissional"],
+    destaque: false
+  },
+  {
+    id: "enactus-uem",
+    titulo: "Enactus UEM",
+    categoria: "Extensão / Empreendedorismo Social",
+    descricao: "Projeto de extensão que desenvolve soluções sustentáveis e empreendedoras para comunidades vulneráveis, formando líderes focados em impacto social.",
+    link: "http://www.pec.uem.br",
+    tags: ["enactus", "empreendedorismo social", "impacto social", "extensão"],
+    destaque: false
+  },
+  {
+    id: "bolsa-permanencia",
+    titulo: "Bolsa Permanência",
+    categoria: "Assistência / Trabalho Acadêmico",
+    descricao: "Bolsa remunerada exclusiva para graduandos (R$ 8,75/hora, até 4h diárias) com atividades práticas executadas nos diversos setores da UEM.",
+    link: "http://www.dca.uem.br",
+    tags: ["bolsa", "remuneração", "permanência", "trabalho", "auxílio"],
+    destaque: true
+  },
+  {
+    id: "repositorio-institucional",
+    titulo: "Repositório Institucional da UEM (RI-UEM)",
+    categoria: "Pesquisa / Acervo",
+    descricao: "Reúne e preserva teses, dissertações, artigos e produções acadêmicas da UEM. Integrado ao Portal de E-books, Periódicos e catálogo online da BCE.",
+    link: "http://www.bce.uem.br",
+    tags: ["repositório", "pesquisa", "teses", "artigos", "e-books", "bce"],
+    destaque: false
+  }
 ];
